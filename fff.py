effb049cb3b9012aa11ac6cd30e93703a7d99c9e
SRI HARI V <153970010+URK23CS1230@users.noreply.github.com>
2024-05-01 22:39:44 +0530

    Update fff.py

---
+++ fff.py
@@ -0,0 +1,36 @@
+git config --global user.email
+git config --global init.defaultBranch main
+git config --list 
+
+git remote add origin 
+git pull re-name branchname
+
+git push main origin 
+
+version control
+      add,commit,push
+
+git commit -m
+
+git commit -m "Fix#1: ufwifgxwf"
+
+
+
+
+
+git remote addd upstream url
+
+git fetch upstream
+
+git merge upstream/main
+
+
+git checkout -b bname
+
+
+
+git request-pull branch repourl
+
+git pull-request(review and merge )
+
+git request-pull -c branch repourl
